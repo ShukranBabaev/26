7d9b2c66bcc624fbd98d5df8f43db5ba947b8336
ShukranBabaev <marina300@bk.ru>
2022-02-24 15:25:22 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] array = new int[20];
+        int s = 0;
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int)(Math.random()* 100);
+            System.out.print(array[i] + " ");
+            s = s + array[i];
+
+
+            }
+        System.out.println();
+        System.out.println(s);
+
+}
+    public void Chek(int s) {
+        if (s%2 == 0){
+            System.out.println("ЧЕТНО");
+        } else {
+            System.out.println("НЕЧЕТНО");
+        }
+
+    }
+}
